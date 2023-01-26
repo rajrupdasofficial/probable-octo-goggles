59daaeaac8d06af4d08a29a1bb6436a32ec5092d
RajrupDasid <rajrupdasid@outlook.com>
2023-01-26 19:11:32 +0530

    profile url added

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from account.views import Registration,Login
+from account.views import Registration,Login,ProfileView
 urlpatterns = [
         path('r/',Registration.as_view(),name="registration"),
         path('l/',Login.as_view(),name="login"),
+        path('p/',ProfileView.as_view(),name="profile")
 ]
