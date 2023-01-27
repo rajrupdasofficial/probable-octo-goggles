22c7366b882efc50c50a3d77b365b397268b131e
RajrupDasid <rajrupdasid@outlook.com>
2023-01-27 13:33:56 +0530

    password change added

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from account.views import Registration,Login,ProfileView
+from account.views import Registration,Login,ProfileView, ChangePassword
 urlpatterns = [
         path('r/',Registration.as_view(),name="registration"),
         path('l/',Login.as_view(),name="login"),
-        path('p/',ProfileView.as_view(),name="profile")
+        path('p/',ProfileView.as_view(),name="profile"),
+        path('cp/',ChangePassword.as_view(),name='password-change')
 ]
