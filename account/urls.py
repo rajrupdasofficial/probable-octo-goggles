6e40e971e1c42ba71adc5e734cd552e17c86acfa
RajrupDasid <rajrupdasid@outlook.com>
2023-01-27 22:18:14 +0530

    password views implemented

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from account.views import Registration,Login,ProfileView, ChangePassword
+from account.views import Registration,Login,ProfileView, ChangePassword,SPRE,UPRV
 urlpatterns = [
         path('r/',Registration.as_view(),name="registration"),
         path('l/',Login.as_view(),name="login"),
         path('p/',ProfileView.as_view(),name="profile"),
-        path('cp/',ChangePassword.as_view(),name='password-change')
+        path('cp/',ChangePassword.as_view(),name='password-change'),
+        path('rp/',SPRE.as_view(),name='password reset'),
+        path('rp/<uid>/<token>/',UPRV.as_view(),name="reset view")
 ]
