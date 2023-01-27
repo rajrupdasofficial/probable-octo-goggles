0de0d7a2ed459114b38de0fd0c24555835da9a7d
RajrupDasid <rajrupdasid@outlook.com>
2023-01-27 13:35:00 +0530

    added remote postgres support

--- dmm/settings.py
+++ dmm/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9papl0x==hmy!zc2f&qws=1$c_5_76_wuh(p$goh$7#4!(mxr0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if config('DEBUG') == "True" else False
+
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'account.User'
@@ -77,13 +79,23 @@ WSGI_APPLICATION = 'dmm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME' : config('SUPABASE_DB_NAME'),
+        'HOST':  config('SUPABASE_HOST'),
+        'USER':  config('DB_USER'),
+        'PASSWORD' : config('SUPABASE_PASSWORD'),
+        'PORT': config('PORT'),
+        'CERT':'prod-ca-2021.crt'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
